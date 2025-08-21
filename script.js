e49727693ddf2ae9f0cec8ce4408b0d66ca60048
Andrew Beveridge <andrew@beveridge.uk>
2025-08-20 23:37:49 -0400

    Added code

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const map = L.map('map').setView([20, 0], 2);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    // TODO: Add data and markers for snowy places
+});
