6701be2a4b46a945683d1aaf1b2b877373d8f584
Andrew Beveridge <andrew@beveridge.uk>
2025-08-20 23:43:13 -0400

    feat: Add snowy places data and display on map

---
+++ data/snowy-places.js
@@ -0,0 +1,10 @@
+const snowyPlaces = [
+    { name: "Zermatt, Switzerland", latitude: 45.9766, longitude: 7.6585 },
+    { name: "Whistler, Canada", latitude: 50.1163, longitude: -122.9574 },
+    { name: "Mount Rainier, USA", latitude: 46.8523, longitude: -121.7604 },
+    { name: "The Andes, Argentina", latitude: -34.6037, longitude: -58.3816 },
+    { name: "Mount Kilimanjaro, Tanzania", latitude: -3.0674, longitude: 37.3556 },
+    { name: "The Himalayas, Nepal", latitude: 27.9881, longitude: 86.9250 },
+    { name: "Antarctica", latitude: -82.8628, longitude: 135.0000 },
+    { name: "Greenland", latitude: 71.7069, longitude: -42.6043 }
+];
--- script.js
+++ script.js
@@ -5,5 +5,16 @@ document.addEventListener('DOMContentLoaded', () => {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    // TODO: Add data and markers for snowy places
+    const snowIcon = L.icon({
+        iconUrl: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0iYmxhY2siIHdpZHRoPSIxOHB4IiBoZWlnaHQ9IjE4cHgiPjxwYXRoIGQ9Ik0wIDBoMjR2MjRIMHoiIGZpbGw9Im5vbmUiLz48cGF0aCBkPSJNMTkgMTN2Nmgydi02aC0yem0tNCAwTDQuNjQgNS42NGwxLjQyLTEuNDJMOC40OSA2LjY1bDEuNDEtMS40MWwyLjgyIDIuODMtMS40MSAxLjQxTDEyIDguNDlsMy41NCAzLjU0IDEuNDEtMS40MSAxLjQyIDEuNDJMMTMgMTN6bS0xLjc2LTcuODhsLjg5LS44OS44OS44OWMtMS43OC0xLjc5LTMuNTQtMy41NS02LjM3LTYuMzctLjUxLjUxLTEuMzMuNTEtMS44NCAwbC0xLjg1LTEuODUgNC4yNC00LjI0aDF6bTMuMjMgMTIuMDJjLTMuNDktMS4zOS02LjI4LTQuMTgtNy42Ny03LjY3bDQuMTItMS42NSA0LjI0IDQuMjQtLjcyLjcxek00IDlsMyAzdjJIODRWOXptMTMuMzYgMS42NGwtMS40MiAxLjQyIDEuNDIgMS40MSAyLjgzLTIuODMtMS40MS0xLjQxem0tNi4zNyA2LjM3bDEuNDEtMS40MiAxLjQyIDEuNDItMi44MyAyLjgyLTEuNDEtMS40MXoiLz48L3N2Zz4=',
+        iconSize: [32, 32],
+        iconAnchor: [16, 32],
+        popupAnchor: [0, -32]
+    });
+
+    snowyPlaces.forEach(place => {
+        L.marker([place.latitude, place.longitude], { icon: snowIcon })
+            .addTo(map)
+            .bindPopup(place.name);
+    });
 });
