7effd806225cf30c27a3878e72c816a84bf42199
Andrew Beveridge <andrew@beveridge.uk>
2025-08-20 23:48:51 -0400

    feat: Improve UI and add more locations

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
     const map = L.map('map').setView([20, 0], 2);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
     }).addTo(map);
 
     const snowIcon = L.icon({
-        iconUrl: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0iYmxhY2siIHdpZHRoPSIxOHB4IiBoZWlnaHQ9IjE4cHgiPjxwYXRoIGQ9Ik0wIDBoMjR2MjRIMHoiIGZpbGw9Im5vbmUiLz48cGF0aCBkPSJNMTkgMTN2Nmgydi02aC0yem0tNCAwTDQuNjQgNS42NGwxLjQyLTEuNDJMOC40OSA2LjY1bDEuNDEtMS40MWwyLjgyIDIuODMtMS40MSAxLjQxTDEyIDguNDlsMy41NCAzLjU0IDEuNDEtMS40MSAxLjQyIDEuNDJMMTMgMTN6bS0xLjc2LTcuODhsLjg5LS44OS44OS44OWMtMS43OC0xLjc5LTMuNTQtMy41NS02LjM3LTYuMzctLjUxLjUxLTEuMzMuNTEtMS44NCAwbC0xLjg1LTEuODUgNC4yNC00LjI0aDF6bTMuMjMgMTIuMDJjLTMuNDktMS4zOS02LjI4LTQuMTgtNy42Ny03LjY3bDQuMTItMS42NSA0LjI0IDQuMjQtLjcyLjcxek00IDlsMyAzdjJIODRWOXptMTMuMzYgMS42NGwtMS40MiAxLjQyIDEuNDIgMS40MSAyLjgzLTIuODMtMS40MS0xLjQxem0tNi4zNyA2LjM3bDEuNDEtMS40MiAxLjQyIDEuNDItMi44MyAyLjgyLTEuNDEtMS40MXoiLz48L3N2Zz4=',
+        iconUrl: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMTIgMmwtMS40MSAxLjQxTDEyIDQuODNsMS40MS0xLjQxTDEyIDJ6bTAtMThsLTEuNDEgMS40MUwxMiAxOS4xN2wxLjQxLTEuNDFMMTIgMjJ6bTctM2wtMS40MS0xLjQxTDE1LjE3IDEybDEuNDIgMS40MUwxOSA5em0tMThsMS40MS0xLjQxTDIuODMgMTJsLTEuNDIgMS40MUwxIDl6bTMuNTQtOC40NmwxLjQxIDEuNDFMNi4zNiA1LjA1IDQuOTUgMy42NHptMTIuMDcgMGwxLjQxIDEuNDFMMTcuNjQgNS4wNWwtMS40MS0xLjQyem0wIDEyLjA3bC0xLjQxIDEuNDFMMTcuNjQgMTguOTVsMS40MS0xLjQyem0tMTIuMDcgMGwtMS40MSAxLjQxTDYuMzYgMTguOTVsMS40MS0xLjQyeiIvPjwvc3ZnPg==',
         iconSize: [32, 32],
         iconAnchor: [16, 32],
         popupAnchor: [0, -32]
     });
 
     snowyPlaces.forEach(place => {
-        L.marker([place.latitude, place.longitude], { icon: snowIcon })
+        L.marker([place.lat, place.lng], { icon: snowIcon })
             .addTo(map)
             .bindPopup(place.name);
     });
+
+    const title = L.control();
+    title.onAdd = function (map) {
+        this._div = L.DomUtil.create('div', 'info');
+        this.update();
+        return this._div;
+    };
+    title.update = function (props) {
+        this._div.innerHTML = '<h4>Year-Round Snow Locations</h4>';
+    };
+    title.addTo(map);
 });
